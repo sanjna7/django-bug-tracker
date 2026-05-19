3da39ee3bdf918cc8ef55c0006fc7e9401390a8a
Sanjna Poojary <poojarysanjna@gmail.com>
2026-05-19 15:04:53 +0530

    Add bug list and detail templates

--- bugs/views.py
+++ bugs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Bug
+
+def bug_list(request):
+  bugs=Bug.objects.filter(status='Open').order_by('created_at')
+  return render(request, 'bugs/bugs_list.html'. {'bugs': bugs})
 
 # Create your views here.
